20873728e6d156476bd8e84c22b618cc3750aa1f
dualism98 <beachsidebearhi@gmail.com>
2021-06-23 14:14:35 +0700

    Added skills module

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -5,7 +5,19 @@ import './Info.css';
 export default function Info(){
     return(
         <div className={'info-container'}>
-            <h2>СКИЛЛЫ</h2>
+            <div>
+                <h2>СКИЛЛЫ</h2>
+                <div className='skill-row'><div id='react' className='current-skill'>REACT (70%)</div></div>
+                <div className='skill-row'><div id='react-native' className='current-skill'>REACT-NATIVE (90%)</div></div>
+                <div className='skill-row'><div id='redux' className='current-skill'>REDUX (50%)</div></div>
+
+                <span style={{marginTop: 10, marginBottom: 10, fontWeight: '500'}}>
+                    Кроме того, имел дело с 
+                    <span style={{color: 'blue'}}> Express JS</span>,
+                    <span style={{color: 'red'}}> MySQL</span>, и немножко
+                    <span style={{color: 'darkorange'}}> Python'ом</span>
+                </span>
+            </div>
         </div>
     )
 }
