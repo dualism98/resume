88cb89be46e67bff9ec3a71ecaa7f9803f7c497d
dualism98 <beachsidebearhi@gmail.com>
2021-06-23 13:49:11 +0700

    Added gradient to header description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import Info from './components/Info/Info';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Info />
     </div>
   );
 }
---
+++ src/components/Info/Info.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Info.css';
+
+
+export default function Info(){
+    return(
+        <div className={'info-container'}>
+            <h2>СКИЛЛЫ</h2>
+        </div>
+    )
+}
